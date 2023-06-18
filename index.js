655646e00a79e16635af803a15d1dc9c40b88a77
Arpitareddot <arpita.reddotapps@gmail.com>
2023-06-18 15:42:15 +0530

    path added

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const PORT = process.env.PORT || 5000;
 const dotenv = require("dotenv");
 const cors = require("cors");
 const dbConnection = require("./db/conn");
-dotenv.config({path: './.env'}); 
+dotenv.config({ path: './.env' }); 
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
